f375451ac0375732689d5242f2dab3420b715bcc
grr37213 <robert.graf@st.oth-regensburg.de>
2017-11-07 10:54:53 +0100

    Header for IBC_Packet started

---
+++ pi/IBP/IBC_Packet.hpp
@@ -0,0 +1,63 @@
+
+#define IBC_PACKET_NOSIZE 0
+
+class IBC_Packet
+{
+	uint8_t id;
+	class Content
+	{
+		uint16_t size;
+		void * data;
+		bool copy;
+		public:
+		Content(void * data, uint16_size = IBC_PACKET_NOSIZE, bool copy = true);
+		~Content();
+	};
+	uint8_t hash;	
+
+public:
+	IBC_Packet(uint8_t id, void * data, uint16_t size = IBC_PACKET_NOSIZE );
+	IBC_Packet(uint8_t id, IBC_Packet::Content content);
+	~IBC_Packet();
+
+
+	//Copy constructor and assignment
+	IBC_Packet(const IBC_Packet&);
+	IBC_Packet& operator= (const IBC_Packet&);
+
+	/**
+	 * @brief Get access to the content data of the packet.
+	 *	Note : Watch the size of the packet contents you can get with size()
+	 *	Note : Also watch if the owner of the Packages data does exist or the data might be corrupted
+	 *
+	 * @return Pointer to the beginning of content-data 
+	 */
+	void * content()const;
+
+	/**
+	 * @return 16Bit integer with number of data in bytes
+	 */
+	uint16_t size()const;
+
+	/**
+	 * @brief Computes a 8Bit long hash value from the contents of the packet.
+	 *
+	 * @return hash value
+	 */
+	uint8_t hash();
+
+	/**
+	 * @brief Set the hash value in the packet.
+	 *
+	 * @param nhash The new has value
+	 */
+	void sethash(uint8_t nhash = hash());
+
+	/**
+	 * @brief Checks the correctness of the hash.
+	 *	Can be used to detect if a received package has been damaged in transmission.
+	 * @return true/false whether hash is correct or not
+	 */
+	bool verify();
+
+};
