42d058472dda3eaa95a28d77754f68c0943c1421
bof31844 <florian.boemmel@oth.st-regensburg.de>
2017-11-16 18:27:31 +0100

    tested and ready for merge

--- pi/IBP/Serial.cpp
+++ pi/IBP/Serial.cpp
@@ -17,36 +17,19 @@ Serial::Serial(std::string devicename)
 {
 
    	fd = open(devicename.c_str(),O_RDWR | O_NOCTTY /*| O_NDELAY*/); // ttyUSB0 is the connected arduino
-																// O_RDWR Read/Write access to serial port
-																// O_NOCTTY - Serial port should not kill the process is a ctrl-c is received
-																// O_NDELAY - Non Blocking Mode                                       
+                                                                        // O_RDWR Read/Write access to serial port
+                                                                        // O_NOCTTY - Serial port should not kill the process is a ctrl-c is received
+                                                                        // O_NDELAY - Non Blocking Mode
 									
-    if(fd == -1)
-	{
-		throw std::runtime_error("openSerialPortArduino: Unable to open " + devicename);	
-	}
+    if(fd == -1){                                                        // existence of file descriptor is assured
 
-	usleep(200000);												// wait some time ... otherwise it will cause problems with the serial port
-	tcflush(fd, TCIOFLUSH);										// clar input / output buffer and discard old data
-																// existence of file descriptor is assured
-	config();
-	
-	send("HelloMSG",8);											// send hello message to arduino
-	char buffer[9];
-	buffer[8] = '\0';
-	
-	std::cout << "|       Sending hello message to arduino...          |" << std::endl;
-	
-	if (recv(buffer, 8) == 8){
-	
-		std::cout << "|        hello message received from arduino         |" << std::endl;
-		std::cout << "|              Serial Port is ready !!               |" << std::endl;
-		std::cout << "+----------------------------------------------------+" << std::endl;	
-	}else{
-	
-		std::cout << "Serial.recv: Error receiving data with error number: " << errno << " Error message: "<< strerror(errno) << std::endl;
-		std::cout << "+----------------------------------------------------+" << std::endl;
-	}
+        throw std::runtime_error("openSerialPortArduino: Unable to open " + devicename);
+    }
+
+    usleep(200000);             // wait some time ... otherwise it will cause problems with the serial port
+    tcflush(fd, TCIOFLUSH);	// clar input / output buffer and discard old data
+
+    config();
 }
 
 Serial::~Serial()
@@ -55,14 +38,8 @@ Serial::~Serial()
 }
 
 void Serial::config()
-{
-	//TODO: FLAGS anschauen, OUTPUT löschen,
-   	
-	//---------- Setting the Attributes of the serial port using termios structure ---------
-	
-	std::cout << "+----------------------------------------------------+" << std::endl;
-	std::cout << "|              Open Serial Port Arduino              |" << std::endl;
-	std::cout << "+----------------------------------------------------+" << std::endl;
+{	
+    //---------- Setting the Attributes of the serial port using termios structure ---------
 
     // Info zu Membern in termios
     /*struct termios
@@ -75,62 +52,47 @@ void Serial::config()
        Array für Sonderzeichen/-funktionen     cc_t c_cc[NCCS]
       }*/
 
-	struct termios SerialPortSettings;
+    struct termios SerialPortSettings;
 
-	tcgetattr(fd, &SerialPortSettings);			// Get the current attributes of the Serial port 
+    tcgetattr(fd, &SerialPortSettings);             // Get the current attributes of the Serial port
 
-    cfsetispeed(&SerialPortSettings,B57600); 	// Set Read Speed as 57600
-    cfsetospeed(&SerialPortSettings,B57600);	// Set Write Speed as 57600
+    cfsetispeed(&SerialPortSettings,B57600);        // Set Read Speed as 57600
+    cfsetospeed(&SerialPortSettings,B57600);        // Set Write Speed as 57600
 
-	SerialPortSettings.c_cflag &= ~PARENB;   	// Disables the Parity Enable bit(PARENB),So No Parity
-	SerialPortSettings.c_cflag &= ~CSTOPB;   	// CSTOPB = 2 Stop bits,here it is cleared so 1 Stop bit
-	SerialPortSettings.c_cflag &= ~CSIZE;	 	// Clears the mask for setting the data size ... enables set own data bits... see next line           
-	SerialPortSettings.c_cflag |=  CS8;      	// Set the data bits = 8                                 
-	
-    //SerialPortSettings.c_cflag &= ~CRTSCTS;         // No Hardware flow Control
+    SerialPortSettings.c_cflag &= ~PARENB;          // Disables the Parity Enable bit(PARENB),So No Parity
+    SerialPortSettings.c_cflag &= ~CSTOPB;          // CSTOPB = 2 Stop bits,here it is cleared so 1 Stop bit
+    SerialPortSettings.c_cflag &= ~CSIZE;           // Clears the mask for setting the data size ... enables set own data bits... see next line
+    SerialPortSettings.c_cflag |=  CS8;             // Set the data bits = 8
     SerialPortSettings.c_cflag |= (CREAD | CLOCAL); // Enable receiver,Ignore Modem Control lines
 		
-		
-    /*SerialPortSettings.c_iflag &= ~(IXON | IXOFF | IXANY);          // Disable XON/XOFF flow control both i/p and o/p
-	SerialPortSettings.c_lflag &= ~(ICANON | ECHO | ECHOE | ISIG); 
-	SerialPortSettings.c_iflag &= ~(ICANON | ECHO | ECHONL | ISIG | IEXTEN);  // Non Cannonical mode
+    cfmakeraw(&SerialPortSettings);                 // Setup Raw-Mode automatically
 
-
-	SerialPortSettings.c_oflag &= ~OPOST;							// No Output Processing
-*/
-    cfmakeraw(&SerialPortSettings);                                 // Setup Raw-Mode automatically
-
-	if((tcsetattr(fd,TCSANOW,&SerialPortSettings)) != 0){ 			// Set the attributes to the termios structure
+    if((tcsetattr(fd,TCSANOW,&SerialPortSettings)) != 0){ 	// Set the attributes to the termios structure
 	
-		std::cout << "|    Serial.config: Error in settting attributes     |" << std::endl;
+        std::cout << "Serial.config: Error in settting attributes" << std::endl;
 		
-	}else{
+    }else{
     
-		std::cout << "| BaudRate = 57600    StopBits = 1   Parity = none   |" << std::endl;
-	}	
+        std::cout << "SerialPort is ready! BaudRate = 57600" << std::endl;
+    }
 	       
-	sleep(3);														// Wait until Arduino restarts !!	
+        sleep(3);   // Wait until Arduino restarts !!
 }
 
 int Serial::send(const void * data , int size) const
 {
 	
-	return write(fd, data, size);									// use write() to send data to port                                            
-																	// "fd"                   - file descriptor pointing to the opened serial port 
-																	// "write_buffer"         - address of the buffer containing data	            
-																	// "sizeof(write_buffer)" - No of bytes to write                               	
-																	// returns the actual bytes written to the port
+        return write(fd, data, size);	// use write() to send data to port
+                                        // "fd"                   - file descriptor pointing to the opened serial port
+                                        // "write_buffer"         - address of the buffer containing data
+                                        // "sizeof(write_buffer)" - No of bytes to write
+                                        // returns the actual bytes written to the port
 }
 
 int Serial::recv(void * data , int maxsize) const
 {
 	
-	return read(fd, data , maxsize); 		// Read the data
-													// Write it to the location at <data>
-													// Write <maxsize> bytes at maximum to not overrun buffersizes
-	/*if(bytes_read < 0)
-	{
-		std::cout << "Serial.recv: Error receiving data with error number: " << errno << " Error message: "<< strerror(errno) << std::endl;
-		}
-	*/
+    return read(fd, data , maxsize); 		// Read the data
+                                                // Write it to the location at <data>
+                                                // Write <maxsize> bytes at maximum to not overrun buffersizes
 }
--- pi/IBP/Serial.test.cpp
+++ pi/IBP/Serial.test.cpp
@@ -1,91 +1,27 @@
 #include "Serial.hpp"
 
 #include <iostream>
-#include <thread>
-#include <csignal>
-#include <sys/time.h>
-
-Serial s ("/dev/ttyUSB0");
-
-void testSerial(){
-	
-	int k = 0;
-	int bytesSend = 0;
-	int bytesRecv = 0;
-	char buffer2[9] = {'1','2','3','4','5','6','7','8'};
-	
-    struct timeval tvalBefore, tvalAfter;
-
-    gettimeofday(&tvalBefore,NULL);
-    tvalAfter.tv_sec = tvalBefore.tv_sec + 5;
-
-    while (tvalBefore.tv_sec < tvalAfter.tv_sec)
-	{
-			
-        bytesSend += s.send(buffer2,8);
-		bytesRecv += s.recv(buffer2,8);
-		buffer2[9] = '\0';
-        //std::cout << "Received: " << buffer2 << std::endl;
-        gettimeofday(&tvalBefore,NULL);
-	}
-
-    //gettimeofday(&tvalAfter,NULL);
-
-	std::cout << "Bytes send: " << bytesSend << "Bytes received: " << bytesRecv << std::endl;
-    //std::cout << "Zeit: " << tvalAfter.tv_usec - tvalBefore.tv_usec << "µS" << tvalBefore.tv_sec - tvalAfter.tv_sec << "s";
-}
-
-void signalHandler (int signum) {
-	
-	if (signum == SIGINT){
-	
-		
-	}
-}
-
 
 int main (int argc , char** argv)
-{
-	//signal(SIGINT,signalHandler);
-	
-	//Serial s ("/dev/ttyUSB0");	 
-	std::thread serial(testSerial);
-	serial.join();	 
-	
-	
-	/*std::cout << "Sending 1235 to Arduino" << std::endl ;
+{	
+    Serial s ("/dev/ttyUSB0");
 
-	std::cout << "Bytes sent : " << s.send("2222", 4) << '\n';
+    std::cout << "Bytes sent : " << s.send("2222", 4) << '\n';
 
-	char buffer[5] = {};
-	
-	s.recv(buffer, 4);
+    char buffer[5] = {};
 
-	buffer[4] = '\0';
+    s.recv(buffer, 4);
+
+    buffer[4] = '\0';
+
+    for(int i = 0; i < 5; i++){
+
+        std::cout << buffer[i] ;
+    }
+
+    std::cout << std::endl << buffer << '\n';
+
+    s.~Serial();
 	
-	for(int i = 0; i < 5; i++)
-	{
-		std::cout << buffer[i] ;
-		}
-	
-	std::cout << std::endl << buffer << '\n';
-	
-	
-	
-	int k = 0;
-	char buffer2[4];
-	
-	while (k < 5)
-	{ 
-		std::cin >> buffer2;
-		s.send(buffer2, 4);
-		s.recv(buffer2, 4);
-		std::cout << buffer2 << std::endl;
-		
-		k++;
-	}
-	*/
-	s.~Serial();
-	
-	return 0;
+    return 0;
 }
