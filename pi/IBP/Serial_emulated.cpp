947043fae984f8c4c4a57b3090f5296b0a80da89
meph <robert.graf24@gmail.com>
2017-11-29 19:23:06 +0100

    Emulated Serial connection for test purposes works

--- pi/IBP/Serial_emulated.cpp
+++ pi/IBP/Serial_emulated.cpp
@@ -19,105 +19,100 @@ void Serial::config()
 int Serial::send(char * data , int size)
 {
 	//write to buffer
-
-	std::cout << "Write : " << std::hex;
-
 	for ( int i = 0; i < size; ++i )
 	{
-			sendbuff.push_back(data[i]);
-			std::cout << '|' <<(0xFF& (unsigned int) data[i]);
-	}	
-	std::cout << std::dec << '\n';
+		sendbuff.push_back(data[i]);
+	}
 
 	//process frames here manually
 	bool stop = false;
-while(!stop)
-{
-	if(sendbuff.empty()) break;
-	switch ((uint8_t) sendbuff.front() )
+	while(!stop)
 	{
-		case 252:
-			std::cout << 252 << "+\n";
-			//normal static request
-			if(sendbuff.size() < 7)
-			{
-				std::cerr << "You did crap 1 !\n";
-				stop = true;
-				break;
-			}
-			//status
-			recvbuff.push_back(0x00);
-			//data
-			recvbuff.push_back(5);
-			recvbuff.push_back(6);
-			recvbuff.push_back(7);
-			recvbuff.push_back(8);
-			recvbuff.push_back(5);
-			recvbuff.push_back(6);
-			recvbuff.push_back(7);
-			recvbuff.push_back(8);
+		if(sendbuff.empty())
+		{
+			stop = true;
+			break;
+		}
+		switch ((uint8_t) sendbuff.front() )
+		{
+			case 252:
+				//normal static request
+				if(sendbuff.size() < 7)
+				{
+					std::cerr << "You did crap 1 !\n";
+					stop = true;
+					break;
+				}
+				//status
+				recvbuff.push_back(0x00);
+				//data
+				recvbuff.push_back(5);
+				recvbuff.push_back(6);
+				recvbuff.push_back(7);
+				recvbuff.push_back(8);
+				recvbuff.push_back(5);
+				recvbuff.push_back(6);
+				recvbuff.push_back(7);
+				recvbuff.push_back(8);
+	
+				//hash
+				recvbuff.push_back(0);
+	
+				std::cout << "Recvbuffer now : \n" << std::hex;
+				for (auto e : recvbuff) std::cout << (uint8_t) e;
+				std::cout << std::dec << '\n';
+	
+				//we sent this stuff and received a response at this point so away with it
+				sendbuff.erase(sendbuff.begin(), sendbuff.begin()+7);
 
-			//hash
-			recvbuff.push_back(0);
+			break;
+			case 253:
+				//construct response from dynamic request
+				if(sendbuff.size() < 3)
+				{
+					std::cerr << "You did crap 2 !\n";
+					stop = true;
+					break;
+				}
+				if(sendbuff.size() < sendbuff[2]+4)
+				{
+					std::cerr << "You did crap 3 !\n";
+					break;
+				}
+				//status
+				recvbuff.push_back(0);
+				//send back the dynamic size 2 times
+				recvbuff.push_back(sendbuff[2]);
+				recvbuff.push_back(sendbuff[2]);
+				//hash	
+				recvbuff.push_back(sendbuff[2]^sendbuff[2]);
+	
+				sendbuff.erase(sendbuff.begin(), sendbuff.begin()+ sendbuff[2]+4);
+			break;
+			case 254:
+				//dynamic response from static request
+				if(sendbuff.size()< 5)
+				{
+					std::cerr << "You did crap 4 !\n";
+					stop = true;
+					break;
+				}
+	
+	
+				//status
+				recvbuff.push_back(0);
+				//size
+				recvbuff.push_back(1);
+				//data
+				recvbuff.push_back(42);
+				//hash
+				recvbuff.push_back(1);
+	
+				sendbuff.erase(sendbuff.begin(), sendbuff.begin()+5);
 
-			std::cout << "Recvbuffer now : \n" << std::hex;
-			for (auto e : recvbuff) std::cout << (uint8_t) e;
-			std::cout << std::dec << '\n';
-
-			//we sent this stuff and received a response at this point so away with it
-			sendbuff.erase(sendbuff.begin(), sendbuff.begin()+7);
-		break;
-		case 253:
-			std::cout << 253 << "+\n";
-			//construct response from dynamic request
-			if(sendbuff.size() < 3)
-			{
-				std::cerr << "You did crap 2 !\n";
-				stop = true;
-				break;
-			}
-			if(sendbuff.size() < sendbuff[2]+4)
-			{
-				std::cerr << "You did crap 3 !\n";
-				break;
-			}
-			//status
-			recvbuff.push_back(0);
-			//send back the dynamic size 2 times
-			recvbuff.push_back(sendbuff[2]);
-			recvbuff.push_back(sendbuff[2]);
-			//hash	
-			recvbuff.push_back(sendbuff[2]^sendbuff[2]);
-
-			sendbuff.erase(sendbuff.begin(), sendbuff.begin()+ sendbuff[2]+3);
-		break;
-		case 254:
-			std::cout << 254 << "+\n";
-			//dynamic response from static request
-			if(sendbuff.size()< 5)
-			{
-				std::cerr << "You did crap 4 !\n";
-				stop = true;
-				break;
-			}
-
-
-			//status
-			recvbuff.push_back(0);
-			//size
-			recvbuff.push_back(1);
-			//data
-			recvbuff.push_back(42);
-			//hash
-			recvbuff.push_back(1);
-
-			sendbuff.erase(sendbuff.begin(), sendbuff.begin()+5);
-		break;
-
-	}
-}	
-
-
+			break;
+		}
+	}	
 	return size;
 }
 
