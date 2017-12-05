79d4a9a7146af9c6f834f0410e2f6b906b2da333
bof31844 <florian.boemmel@st.oth-regensburg.de>
2017-12-05 11:34:45 +0100

    updated arbeitszeiten

--- pi/GUI/ControllGUI/pathsandconstans.h
+++ pi/GUI/ControllGUI/pathsandconstans.h
@@ -4,13 +4,22 @@
 #include <QString>
 
 // For MAC
-
+/*
 const QString errorImage = "/Users/florianboemmel/DT_WS1718_02_StarCar/pi/GUI/ControllGUI/Pics/alert_red.png";
 const QString warningImage = "/Users/florianboemmel/DT_WS1718_02_StarCar/pi/GUI/ControllGUI/Pics/alert_orange.png";
 const QString noWarOrErrImage = "/Users/florianboemmel/DT_WS1718_02_StarCar/pi/GUI/ControllGUI/Pics/alert_green.png";
 const QString whiteWarningImage = "/Users/florianboemmel/DT_WS1718_02_StarCar/pi/GUI/ControllGUI/Pics/alert_white.png";
 const QString startImage = "/Users/florianboemmel/DT_WS1718_02_StarCar/pi/GUI/ControllGUI/Pics/start.png";
 const QString exitImage = "/Users/florianboemmel/DT_WS1718_02_StarCar/pi/GUI/ControllGUI/Pics/exit.png";
+*/
+// For WIN
+
+const QString errorImage = "C:/Users/Flo/Desktop/DT_WS1718_02_StarCar/pi/GUI/ControllGUI/Pics/alert_red.png";
+const QString warningImage = "C:/Users/Flo/Desktop/DT_WS1718_02_StarCar/pi/GUI/ControllGUI/Pics/alert_orange.png";
+const QString noWarOrErrImage = "C:/Users/Flo/Desktop/DT_WS1718_02_StarCar/pi/GUI/ControllGUI/Pics/alert_green.png";
+const QString whiteWarningImage = "C:/Users/Flo/Desktop/DT_WS1718_02_StarCar/pi/GUI/ControllGUI/Pics/alert_white.png";
+const QString startImage = "C:/Users/Flo/Desktop/DT_WS1718_02_StarCar/pi/GUI/ControllGUI/Pics/start.png";
+const QString exitImage = "C:/Users/Flo/Desktop/DT_WS1718_02_StarCar/pi/GUI/ControllGUI/Pics/exit.png";
 
 
 #endif // PATHSANDCONSTANS_H
