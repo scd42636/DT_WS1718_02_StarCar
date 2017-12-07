55ea673b7bc55ffaadfa70343fa86298f721be28
grr37213 <robert.graf@st.oth-regensburg.de>
2017-12-07 23:05:31 +0100

    committed myself to ease the use on the arduino side as well and started writing tool functions and frame for protocoll use

--- uno/communicationUNO.ino
+++ uno/communicationUNO.ino
@@ -4,59 +4,208 @@ void setup() {
 
 }
 
+char createSH(char SIZE)
+{
+	return ((SIZE ^ (SIZE >> 2) ^ (SIZE >> 4) ^ (SIZE >> 6)) & 0x03) << 2;
+}
+
+char createHH_response(char STAT)
+{
+	return (STAT ^ (STAT >> 2)) & 0x03;
+}
+
+char createHH_request(char MID , char STAT)
+{
+	return (MID ^ (MID >> 2) ^ (MID >> 4) ^ (MID >> 6) ^ SIZE ^ (SIZE >> 2)) & 0x03;
+}
+
+char createDH(char* data, int size, char in)
+{
+	int i;
+	for(i = 0; i < size ; i++)
+	{
+		in ^= data[i];
+	}
+	return in;
+}
+
+char createDH(char* data, int size)
+{
+	createDH(data, size, 0);
+}
+
+struct serialfield
+{
+	char IN_MID, IN_STAT, IN_SH, IN_HH, IN_DH;
+
+	char THIS_STAT = 0;
+	char CALC_DH;
+};
+
+void init_serialfield(serialfield *s)
+{
+	*s = {0,0,0,0,0,0,0};
+}
+
+/**
+* @brief Errorhandling on Datahashes
+*	Checks the equality of datahash and calculated hash and reacts to a mismatch.
+*	Call this function when the next byte to receive is the to receive data hash.
+*
+* @param THIS_STAT	The Address where the IBP-Status of this slave is stored in the lowest 4 bits.
+* @param CALC_DH	The datahash which was calculated on this slave.
+*
+* @return	true = success , false = failure : All internal actions should already be performed by this function any way, so this return serves only convenience. 
+*/
+bool checkDH(serialfield *sf)
+{
+	sf->IN_DH = Serial.read();
+	if(sf->IN_DH == sf->CALC_DH)	return true;
+	
+	//set status bit for failed data hash
+	sf->THIS_STAT |= 0x01;
+	return false;
+}
+
+bool checkSH(serialfield *sf)
+{
+	if(createSH(sf->IN_SIZE) == IN_SH) return true;
+
+	//set status bit for failed size hash
+	sf->THIS_STAT |= 0x02;
+	return false;
+}
+
+bool checkHH_request(serialfield *sf)
+{
+	if(createHH_request(sf->IN_MID, sf->IN_STAT) == sf->IN_HH)	return true;
+
+	//set status bit for failed headhash
+	sf->THIS_STAT |= 0x04;
+	return false;
+}
+
+bool possibleError(serialfield *sf)
+{
+
+}
+
+void newrequest(serialfield *sf)
+{
+		sf->IN_MID = recv(); // read first byte supposed to be id
+		sf->IN_STAT = recv(); //read second byte supposed to be status and hashes
+
+		sf->IN_SH = (sf->IN_STAT & 0x0C) >> 2;
+		sf->IN_HH = sf->IN_STAT & 0x03;
+		sf->IN_STAT = sf->IN_STAT >> 4;
+}
+
+//TODO THIS_sendmultiple THIS_recvmultiple overall own send functions
+
+void recv(char * c, int length)
+{
+	if(Serial.available()>=length)
+	{
+		int i;
+		for(i = 0; i < length; i++)
+		{
+			c[i] = Serial.read();
+		}
+	}
+}
+
+char recv()
+{
+	char c;
+	recv(&c, 1);
+	return c;
+}
+
+char
+
 void loop() {
 
-  while (Serial.available() > 0 ) {
-
-    char id = Serial.read(); // read first byte supposed to be id
-	char status = Serial.read(); //read second byte supposed to be status and hashes
+	serialfield SF;
+	init_serialfield(&SF);
 	
-	//TODO check status
-	//TODO check hashes
-
-	switch(id)
+	while (1) 
 	{
-		case 252:
 
-		char buff[4];
-			for(int i = 0 , i < 4; i++)
-			{	
-				buff[i] = Serial.read();
-			}
+		newrequest
 
-			//check data hash here
-			char hash = buff[0] ^ buff[1] ^ buff[2] ^ buff[3];
+		//TODO check status
+		//TODO check hashes
 
-			char receivedhash = Serial.read();
+		switch(MID)
+		{
+			case 252:
+				char buff[4];
+				for(int i = 0 , i < 4; i++)
+				{
+					buff[i] = Serial.read();
+				}
 
-			if(hash != receivedhash)
-			{
-				//TODO errorhandling
-			}
+				//check data hash here
+				CALC_DH = createDH(buff, 4);
+				checkDH(&THIS_STAT, CALC_DH);
 
-			//send answer from here
-		
-			//construct status and hashes
-			Serial.write(0x00);
+				//send answer from here
+				//construct status and hashes as header
+				//here for a static comm we need no sizehash
+				Serial.write((THIS_STAT << 4) & createHH(STAT));
 
-			//Datagramm
-			Serial.write(5);
-			Serial.write(6);
-			Serial.write(7);
-			Serial.write(8);
+				//Datagramm
+				Serial.write(5);
+				Serial.write(6);
+				Serial.write(7);
+				Serial.write(8);
 
-			Serial.write(5);
-			Serial.write(6);
-			Serial.write(7);
-			Serial.write(8);
+				Serial.write(5);
+				Serial.write(6);
+				Serial.write(7);
+				Serial.write(8);
 
-			//hash
-			Serial.write(0);
-		break;	
+				//set hash
+				DH = 0;
+			break;
+
+			case 253:
+				char size = Serial.read();
+
+				//if you are dynamic you have to check the sizehash
+				checkSH(size);
+
+				char * buff = new char [size];
+
+				int i;
+				for(i = 0; i < size ; i++)
+				{
+					buff[i] = Serial.read();
+				}
+
+				//check data hash
+				CALC_DH = createDH(buff, size);
+				checkDH(&THIS_STAT, CALC_DH);
+
+				//do smth with received data
+
+				//status and hashes
+				Serial.write(0);
+
+				//data
+				Serial.write(2);
+				Serial.write(2);
+
+				char x [2] = {2, 2};
+				DH = createDH(x , 2);
+			break;
+
+			case 254:
+				
+			break;
+		}
+
+		//send hash
+		Serial.write(DH);
 	}
-
-    Serial.write(incomingByte);        // send it back
-
-  }
-
 }
