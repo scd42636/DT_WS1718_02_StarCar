0e216a20088430cd031a944481468bb857045182
hus37652 <32652091+hus37652@users.noreply.github.com>
2017-12-12 16:16:07 +0100

    Change Sensors

--- uno/StarCarUno/MagnetSensor.ino
+++ uno/StarCarUno/MagnetSensor.ino
@@ -1,13 +1,14 @@
-
 #include <Wire.h>
+#include <EEPROM.h>
 
 // configure the compass as reqiured
-#define OSR 0b00               // over sampling rate set to 512. 0b01 is 256, 0b10 is 128 and 0b11 is 64
-#define RNG 0b00               // Full Scale set to +/- 2 Gauss, 0b01 is +/- 8.
-#define ODR 0b00               // output data rate set to 10Hz, 0b01 is 50Hz, 0b10 is 100Hz, 0b11 is 200Hz
-#define MODE 0b01              // continuous measurement mode, 0b00 is standby
-#define CR2 0b00000000         // control register 2: disable soft reset and pointer rollover, interrupt pin not enabled
+#define OSR 0b00 // over sampling rate set to 512. 0b01 is 256, 0b10 is 128 and 0b11 is 64
+#define RNG 0b00 // Full Scale set to +/- 2 Gauss, 0b01 is +/- 8.
+#define ODR 0b00 // output data rate set to 10Hz, 0b01 is 50Hz, 0b10 is 100Hz, 0b11 is 200Hz
+#define MODE 0b01 // continuous measurement mode, 0b00 is standby
+#define CR2 0b00000000 // control register 2: disable soft reset and pointer rollover, interrupt pin not enabled
 #define RESET 0b00000001
+
 
 // Reference the I2C Library
 #define HMC5883L_Address 0x0D
@@ -17,117 +18,143 @@
 
 int error = 0;
 float Scale;
+int c;
+
+float ConvertToPositive (float convert){
+  if (convert > 180){
+    convert = 180-convert;
+    }
+  if (convert < 0){
+    convert = 0-convert;
+    c = 1; //1 entspricht negativer Zahl
+    }
+   else{
+    c = 0;
+    }
+    return convert;
+  
+}
 
 // Out setup routine, here we will configure the microcontroller and compass.
 void setup()
 {
-  // Initialize the serial port.
-  Serial.begin(9600);
-  
-  Wire.begin(); // Start the I2C interface.
- // Check that a device responds at the compass address - don't continue if it doesn't - 
-  do {
-      delay(100);
-      Wire.beginTransmission(HMC5883L_Address);
-      error = Wire.endTransmission();
-  } while (error);
+// Initialize the serial port.
+Serial.begin(9600);
 
-  // configure the control registers using static settings above
-  // compass autoranges, but starts in the mode given
-  Write(ControlRegister1, MODE);
-  Write(ControlRegister2, CR2);
- // Write(SetResetRegister, RESET); 
-  switch (MODE & 0b00010000){
-   case 0b00010000: 
-     Scale = (float) 8 / (float) 32768;
-     break;
-   default:
-     Scale = (float) 2 / (float) 32768;
-     break;
-  }
+Wire.begin(); // Start the I2C interface.
+// Check that a device responds at the compass address - don't continue if it doesn't -
+do {
+delay(100);
+Wire.beginTransmission(HMC5883L_Address);
+error = Wire.endTransmission();
+} while (error);
+
+// configure the control registers using static settings above
+// compass autoranges, but starts in the mode given
+Write(ControlRegister1, MODE);
+Write(ControlRegister2, CR2);
+// Write(SetResetRegister, RESET);
+switch (MODE & 0b00010000){
+case 0b00010000:
+Scale = (float) 8 / (float) 32768;
+break;
+default:
+Scale = (float) 2 / (float) 32768;
+break;
+}
 }
 
 // Our main program loop.
 void loop()
 {
-  int XAxis, YAxis;
-  float Xscale, Yscale; 
-  
-  // Retrive the raw values from the compass (not scaled).
- Wire.beginTransmission(HMC5883L_Address);
- Wire.write(DataRegisterBegin);
- Wire.endTransmission();
- 
- Wire.beginTransmission(HMC5883L_Address);
- Wire.requestFrom(HMC5883L_Address, 9);
- if(9<=Wire.available())
- {
-  XAxis = Wire.read()<<8;  //MSB
-  XAxis |= Wire.read();    //LSB
-  YAxis = Wire.read()<<8;
-  YAxis |= Wire.read();
- }
- 
-  // Retrived the scaled values from the compass (scaled to the configured scale).
-  Xscale = XAxis * Scale;
-  Yscale = YAxis * Scale;
+int XAxis, YAxis;
+float Xscale, Yscale;
 
-  // Calculate heading when the magnetometer is level, then correct for signs of axis.
-  // relative to a magnetometer mounted with 'Y' axis pointing in the direction to be measured
-  // and with 'X' pointing to the right from the perspective of the operator facing in the +Y direction
-  // heading (degrees): 0 = +X, 90 = +Y, 180 = -X, 270 = -Y
-  float heading = atan2(Yscale, Xscale);
- 
-  // Once you have your heading, you must then add your 'Declination Angle', which is the 'Error' of the magnetic field in your location.
-  float declinationAngle = 0.05515;  // compass reads magnetic north for those who remember how to read maps and adjust for declination
-  heading += declinationAngle;
-  
-  // Correct for when signs are reversed.
-  if(heading < 0)
-    heading += 2*PI;
-    
-  // Check for wrap due to addition of declination.
-  if(heading > 2*PI)
-    heading -= 2*PI;
-   
-  // Convert radians to degrees for readability.
-  float headingDegrees = heading * 180/M_PI; 
+// Retrive the raw values from the compass (not scaled).
+Wire.beginTransmission(HMC5883L_Address);
+Wire.write(DataRegisterBegin);
+Wire.endTransmission();
 
-  // Output the data via the serial port.
-  Output(heading, headingDegrees);
+Wire.beginTransmission(HMC5883L_Address);
+Wire.requestFrom(HMC5883L_Address, 9);
+if(9<=Wire.available())
+{
+XAxis = Wire.read()<<8; //MSB
+XAxis |= Wire.read(); //LSB
+YAxis = Wire.read()<<8;
+YAxis |= Wire.read();
+}
 
-  // 1. delay the application by 100ms to allow the loop to run at 10Hz (default bandwidth for the HMC5883L)
-  delay(1000);
+// Retrived the scaled values from the compass (scaled to the configured scale).
+Xscale = XAxis * Scale;
+Yscale = YAxis * Scale;
+
+// Calculate heading when the magnetometer is level, then correct for signs of axis.
+// relative to a magnetometer mounted with 'Y' axis pointing in the direction to be measured
+// and with 'X' pointing to the right from the perspective of the operator facing in the +Y direction
+// heading (degrees): 0 = +X, 90 = +Y, 180 = -X, 270 = -Y
+float heading = atan2(Yscale, Xscale);
+
+// Once you have your heading, you must then add your 'Declination Angle', which is the 'Error' of the magnetic field in your location.
+float declinationAngle = 0.05515; // compass reads magnetic north for those who remember how to read maps and adjust for declination
+heading += declinationAngle;
+
+// Correct for when signs are reversed.
+if(heading < 0)
+heading += 2*PI;
+
+// Check for wrap due to addition of declination.
+if(heading > 2*PI)
+heading -= 2*PI;
+
+// Convert radians to degrees for readability.
+float headingDegrees = heading * 180/M_PI;
+
+headingDegrees = ConvertToPositive (headingDegrees);
+
+// Output the data via the serial port.
+Output(c, headingDegrees);
+
+// 1. delay the application by 100ms to allow the loop to run at 10Hz (default bandwidth for the HMC5883L)
+delay(1000);
 }
 
 void Write(int address, int data)
 {
-  Wire.beginTransmission(HMC5883L_Address);
-  Wire.write(address);
-  Wire.write(data);
-  Wire.endTransmission();
+Wire.beginTransmission(HMC5883L_Address);
+Wire.write(address);
+Wire.write(data);
+Wire.endTransmission();
 }
+
+
 // Output the data down the serial port.
-void Output(float heading, float headingDegrees)
-{   
-   Serial.print(" Heading: ");
-   Serial.print(headingDegrees);
-   Serial.print(" Bearing: ");
-   Serial.print(bearingDegrees(headingDegrees));
-   Serial.println(" (Degrees)");
+
+void Output(int c, float headingDegrees)
+{
+//Write to EEPROM
+EEPROM.write(0, c);
+EEPROM.write(1, headingDegrees);
+int x = EEPROM.read (0);
+int y = EEPROM.read (1);
+Serial.print(x, DEC);
+Serial.println();
+Serial.print(y, DEC);
+Serial.println();
+
 }
 
-  // Cacluate bearing from heading.
-  // relative to a magnetometer mounted with 'Y' axis pointing in the direction to be measured
-  // and with 'X' pointing to the right from the perspective of the operator facing in the +Y direction
-  // bearing 0 = Y pointing North, 90 = Y pointing E, 180 = Y pointing S, 270 = Y pointing W
-  float bearingDegrees(float headingDegrees) {
-     
-     float bearing = headingDegrees - 90;
-     if (bearing < 0)
-     {
-      bearing += 360;
-     }
-     return bearing;
-  }
+// Cacluate bearing from heading.
+// relative to a magnetometer mounted with 'Y' axis pointing in the direction to be measured
+// and with 'X' pointing to the right from the perspective of the operator facing in the +Y direction
+// bearing 0 = Y pointing North, 90 = Y pointing E, 180 = Y pointing S, 270 = Y pointing W
+float bearingDegrees(float headingDegrees) {
 
+float bearing = headingDegrees - 90;
+if (bearing < 0)
+{
+bearing += 360;
+}
+return bearing;
+
+}
--- uno/StarCarUno/beschleunigung.ino
+++ uno/StarCarUno/beschleunigung.ino
@@ -1,56 +1,121 @@
 #include <Wire.h>
+#include <EEPROM.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_ADXL345_U.h>
 
+/* Assign a unique ID to this sensor at the same time */
+Adafruit_ADXL345_Unified accel = Adafruit_ADXL345_Unified(12345);
 
-#define ADXL345_Address         0x00
-#define ADXL345_REG_BW_RATE     0x2C   
-#define ADXL345_REG_DATA_FORMAT 0x31
-#define ADXL345_REG_POWER_CTL   0x2D
-#define ADXL345_REG_DATAX0      0x32    // X-axis data 0
-#define ADXL345_REG_DATAY0      0x34    // Y-axis data 0
-#define ADXL345_MG2G_MULTIPLIER 0.004
+int c;
+
+float ConvertToPositive (float convert){
+  if (convert < 0){
+    convert = 0-convert;
+    c = 1; //1 entspricht negativer Zahl
+    }
+   else{
+    c = 0;
+    }
+    return convert; 
+}
 
 void setup(void) 
 {
-  Serial.begin(9600);  
-  /* Initialise the sensor */
-  Wire.begin();
-  Wire.beginTransmission(ADXL345_Address);
-  Wire.endTransmission();
+  Serial.begin(9600);
+  Serial.println("Accelerometer Test"); Serial.println("");
   
-  // Set the accuracy 
-  Write(ADXL345_REG_DATA_FORMAT, 0b00);
-  // enable measurements
-  Write(ADXL345_REG_POWER_CTL, 0x08); 
- }
+  /* Initialise the sensor */
+  if(!accel.begin())
+  {
+    /* There was a problem detecting the ADXL345 ... check your connections */
+    Serial.println("Ooops, no ADXL345 detected ... Check your wiring!");
+    while(1);
+  }
+
+  /* Set the range to whatever is appropriate for your project */
+  accel.setRange(ADXL345_RANGE_2_G);
+  // displaySetRange(ADXL345_RANGE_8_G);
+  // displaySetRange(ADXL345_RANGE_4_G);
+  // displaySetRange(ADXL345_RANGE_2_G);
+  
+  Serial.println("");
+}
 
 void loop(void) 
 {
-  int Xvalue, Yvalue;
-  float Xaccel, Yaccel;
+  /* Get a new sensor event */ 
+  sensors_event_t event; 
+  accel.getEvent(&event);
+ 
+  /* Display the results (acceleration is measured in m/s^2) */
+  //Serial.print("X: "); Serial.print(event.acceleration.x); Serial.print("  ");
+  //Serial.print("Y: "); Serial.print(event.acceleration.y); Serial.print("  ");
 
- Xvalue = Read(ADXL345_REG_DATAX0);
- Yvalue = Read(ADXL345_REG_DATAY0);
+  //Write to EEPROM 
+  
+float Xaccel = event.acceleration.x;
+Serial.print(Xaccel);
+Serial.println();
+Xaccel = ConvertToPositive(Xaccel);
 
- Xaccel = Xvalue * 9.81;
- Yaccel = Yvalue * 9.81;
-  
-  
+EEPROM.put(9, Xaccel);
+float test = EEPROM.get(9, Xaccel);
+Serial.print(test);
+Serial.println();
+delay (1000);
+Serial.print("STOPP");
+
+Serial.println();
+
+Serial.print(Xaccel);
+Serial.println();
+Serial.print("Ende");
+Serial.println();
+//fehler nach Ende
+EEPROM.write(0, c);
+EEPROM.write(1, Xaccel);
+int uw = EEPROM.read (0);
+int xt = EEPROM.read (1);
+Serial.print(uw, DEC);
+Serial.println();
+Serial.print("Positiv oder negativ");
+Serial.println();
+Serial.print(xt, DEC);
+Serial.println();
+Serial.print("Beschleunigung in X");
+Serial.println();
+
+float Yaccel = event.acceleration.y;
+Serial.print(Yaccel);
+Serial.println();
+Yaccel = ConvertToPositive(Yaccel);
+
+EEPROM.put(10, Yaccel);
+float test2 = EEPROM.get(10, Yaccel);
+Serial.print(test2);
+Serial.println();
+delay (1000);
+Serial.print("STOPP");
+
+Serial.println();
+
+Serial.print(Yaccel);
+Serial.println();
+Serial.print("Ende");
+Serial.println();
+//fehler nach Ende
+EEPROM.write(0, c);
+EEPROM.write(1, Yaccel);
+int u = EEPROM.read (0);
+int xs = EEPROM.read (1);
+Serial.print(u, DEC);
+Serial.println();
+Serial.print("Positiv oder negativ");
+Serial.println();
+Serial.print(xs, DEC);
+Serial.println();
+Serial.print("Beschleunigung in Y");
+Serial.println();
+
   delay(500);
-}
-int Read(int address)
-{
-  int variable;
-  Wire.beginTransmission(ADXL345_Address);
-  Wire.write(address);
-  Wire.endTransmission();
-  Wire.requestFrom(ADXL345_Address, 2);
-  variable = Wire.read() | (Wire.read() << 8);
-  return(variable);
-}
-void Write(int address, int data)
-{
-  Wire.beginTransmission(ADXL345_Address);
-  Wire.write(address);
-  Wire.write(data);
-  Wire.endTransmission();
 }
