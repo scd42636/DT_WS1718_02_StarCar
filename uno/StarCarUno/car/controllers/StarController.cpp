740487c19bace11d4d0c6c6137ec9c09b6b2ca5f
d.scharnagl <dominik.scharnagl@traeger.de>
2018-01-05 13:05:59 +0100

    Cleaned up code.

--- uno/StarCarUno/car/controllers/StarController.cpp
+++ uno/StarCarUno/car/controllers/StarController.cpp
@@ -59,22 +59,48 @@ void StarController::Task(StarCar* car)
                 this->modeIsActive = true;
             }
 
+            sbyte_t oldDirectionValue = car->getDirection();
+
             short_t leftX = this->xboxController->getAnalogHat(AnalogHatEnum::LeftHatX);
-            car->setDirection(((float_t)leftX / 32768.0) * 100);
+            sbyte_t newDirectionValue = ((float_t)leftX / 32768.0) * 100;
+
+            if (oldDirectionValue != newDirectionValue) {
+                car->setDirection(newDirectionValue);
+
+                ////Serial.print("Direction: ");
+                ////Serial.println(newDirectionValue);
+            }
 
             if (this->mode == StarControllerMode::Hat) {
+                sbyte_t oldSpeedValue = car->getSpeed();
+
                 short_t rightY = this->xboxController->getAnalogHat(AnalogHatEnum::RightHatY);
-                car->setSpeed(((float_t)rightY / 32768.0) * 100);
+                sbyte_t newSpeedValue = ((float_t)rightY / 32768.0) * 100;
+
+                if (oldSpeedValue != newSpeedValue) {
+                    car->setSpeed(newSpeedValue);
+
+                    ////Serial.print("Speed: ");
+                    ////Serial.println(newSpeedValue);
+                }
             }
             else {
+                sbyte_t oldSpeedValue = car->getDirection();
+
                 short_t rightY = this->xboxController->getButtonPress(ButtonEnum::R2);
                 rightY += -this->xboxController->getButtonPress(ButtonEnum::L2);
 
-                car->setSpeed(((float_t)rightY / 255.0) * 100);
+                sbyte_t newSpeedValue = ((float_t)rightY / 255.0) * 100;
+
+                if (oldSpeedValue != newSpeedValue) {
+                    car->setSpeed(newSpeedValue);
+
+                    ////Serial.print("Speed: ");
+                    ////Serial.println(newSpeedValue);
+                }
             }
 
             if (this->xboxController->getButtonClick(ButtonEnum::X)) {
-                Serial.println("X");
                 StarCarEngineMode engineMode = car->getEngineMode();
 
                 if (engineMode == StarCarEngineMode::CarEngineMode_Off) {
@@ -93,163 +119,13 @@ void StarController::Task(StarCar* car)
                 }
             }
 
-            if (this->xboxController->getButtonClick(ButtonEnum::B)) {
+            if (this->xboxController->getButtonClick(ButtonEnum::B))
                 car->setEngineMode(StarCarEngineMode::CarEngineMode_Off);
-                Serial.println("B");
-            }
         }
         else {
             this->xboxController->setLedOff();
         }
     }
-
-    ////if (this->xboxController->getButtonPress(L2)
-    ////    || this->xboxController->getButtonPress(R2)) {
-    ////    #if TEST
-    ////    Serial.print("L2: ");
-    ////    Serial.print(this->xboxController->getButtonPress(L2));
-
-    ////    Serial.print("\tR2: ");
-    ////    Serial.println(this->xboxController->getButtonPress(R2));
-    ////    #endif
-
-    ////    this->xboxController->setRumbleOn(
-    ////        this->xboxController->getButtonPress(L2),
-    ////        this->xboxController->getButtonPress(R2));
-    ////}
-    ////else {
-    ////    this->xboxController->setRumbleOn(0, 0);
-    ////}
-
-    ////if (this->xboxController->getAnalogHat(LeftHatX) > 7500
-    ////    || this->xboxController->getAnalogHat(LeftHatX) < -7500
-    ////    || this->xboxController->getAnalogHat(LeftHatY) > 7500
-    ////    || this->xboxController->getAnalogHat(LeftHatY) < -7500
-    ////    || this->xboxController->getAnalogHat(RightHatX) > 7500
-    ////    || this->xboxController->getAnalogHat(RightHatX) < -7500
-    ////    || this->xboxController->getAnalogHat(RightHatY) > 7500
-    ////    || this->xboxController->getAnalogHat(RightHatY) < -7500) {
-    ////    if (this->xboxController->getAnalogHat(LeftHatX) > 7500
-    ////        || this->xboxController->getAnalogHat(LeftHatX) < -7500) {
-    ////        #if TEST
-    ////        Serial.print(F("LeftHatX: "));
-    ////        Serial.print(this->xboxController->getAnalogHat(LeftHatX));
-    ////        Serial.print("\t");
-    ////        #endif
-    ////    }
-    ////    if (this->xboxController->getAnalogHat(LeftHatY) > 7500
-    ////        || this->xboxController->getAnalogHat(LeftHatY) < -7500) {
-    ////        #if TEST
-    ////        Serial.print(F("LeftHatY: "));
-    ////        Serial.print(this->xboxController->getAnalogHat(LeftHatY));
-    ////        Serial.print("\t");
-    ////        #endif
-    ////    }
-    ////    if (this->xboxController->getAnalogHat(RightHatX) > 7500
-    ////        || this->xboxController->getAnalogHat(RightHatX) < -7500) {
-    ////        #if TEST
-    ////        Serial.print(F("RightHatX: "));
-    ////        Serial.print(this->xboxController->getAnalogHat(RightHatX));
-    ////        Serial.print("\t");
-    ////        #endif
-    ////    }
-    ////    if (this->xboxController->getAnalogHat(RightHatY) > 7500
-    ////        || this->xboxController->getAnalogHat(RightHatY) < -7500) {
-    ////        #if TEST
-    ////        Serial.print(F("RightHatY: "));
-    ////        Serial.print(this->xboxController->getAnalogHat(RightHatY));
-    ////        #endif
-    ////    }
-    ////    #if TEST
-    ////    Serial.println();
-    ////    #endif
-    ////}
-
-    ////if (this->xboxController->getButtonClick(UP)) {
-    ////    this->xboxController->setLedOn(LED1);
-    ////    #if TEST
-    ////    Serial.println(F("Up"));
-    ////    #endif
-    ////}
-    ////if (this->xboxController->getButtonClick(DOWN)) {
-    ////    this->xboxController->setLedOn(LED4);
-    ////    #if TEST
-    ////    Serial.println(F("Down"));
-    ////    #endif
-    ////}
-    ////if (this->xboxController->getButtonClick(LEFT)) {
-    ////    this->xboxController->setLedOn(LED3);
-    ////    #if TEST
-    ////    Serial.println(F("Left"));
-    ////    #endif
-    ////}
-    ////if (this->xboxController->getButtonClick(RIGHT)) {
-    ////    this->xboxController->setLedOn(LED2);
-    ////    #if TEST
-    ////    Serial.println(F("Right"));
-    ////    #endif
-    ////}
-
-    ////if (this->xboxController->getButtonClick(START)) {
-    ////    this->xboxController->setLedMode(ALTERNATING);
-    ////    #if TEST
-    ////    Serial.println(F("Start"));
-    ////    #endif
-    ////}
-    ////if (this->xboxController->getButtonClick(BACK)) {
-    ////    this->xboxController->setLedBlink(ALL);
-    ////    #if TEST
-    ////    Serial.println(F("Back"));
-    ////    #endif
-    ////}
-    ////if (this->xboxController->getButtonClick(L3)) {
-    ////    #if TEST
-    ////    Serial.println(F("L3"));
-    ////    #endif
-    ////}
-    ////if (this->xboxController->getButtonClick(R3)) {
-    ////    #if TEST
-    ////    Serial.println(F("R3"));
-    ////    #endif
-    ////}
-
-    ////if (this->xboxController->getButtonClick(L1)) {
-    ////    #if TEST
-    ////    Serial.println(F("L1"));
-    ////    #endif
-    ////}
-    ////if (this->xboxController->getButtonClick(R1)) {
-    ////    #if TEST
-    ////    Serial.println(F("R1"));
-    ////    #endif
-    ////}
-    ////if (this->xboxController->getButtonClick(XBOX)) {
-    ////    this->xboxController->setLedMode(ROTATING);
-    ////    #if TEST
-    ////    Serial.println(F("Xbox"));
-    ////    #endif
-    ////}
-
-    ////if (this->xboxController->getButtonClick(A)) {
-    ////    #if TEST
-    ////    Serial.println(F("A"));
-    ////    #endif
-    ////}
-    ////if (this->xboxController->getButtonClick(B)) {
-    ////    #if TEST
-    ////    Serial.println(F("B"));
-    ////    #endif
-    ////}
-    ////if (this->xboxController->getButtonClick(X)) {
-    ////    #if TEST
-    ////    Serial.println(F("X"));
-    ////    #endif
-    ////}
-    ////if (this->xboxController->getButtonClick(Y)) {
-    ////    #if TEST
-    ////    Serial.println(F("Y"));
-    ////    #endif
-    ////}
 }
 
 // ---------- Protected methods ----------
